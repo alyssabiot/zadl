5876b1d0841186c5034cb261aef0a46b75c2317f
Isabelle Bonnard <isabellebonnard@MacBook-Pro-de-Isabelle.local>
2016-12-12 14:51:56 +0100

    seed and user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,26 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
-    # Compte les events auquels participe le current user sans en être organisateur
     @booking = Booking.new
-    @bookings_as_participant = @user.bookings.joins(:event).where("events.active = ? AND bookings.cancelled = ?", true, false)
-    @bookings_cancelled =
+    # Tous les bookings en cours de l'user (yc. lorsqu'il organise ou lorsqu'il est en liste d'attente)
+    @all_bookings_active = @user.bookings.joins(:event).where("events.active = ? AND bookings.cancelled = ?", true, false)
+    # Tous les bookings en cours de l'user à venir
+    @bookings_active_to_come = @all_bookings_active.select do |booking|
+      booking.starts_at >= Date.now
+    end
+    # Tous les bookings en cours de l'user passés
+    @bookings_active_past = @all_bookings_active.select do |booking|
+      booking.starts_at < Date.now
+    end
+    # Tous les bookings de l'user où l'event a été annulé
+    @bookings_event_cancelled = @user.bookings.joins(:event).where("events.active = ?", false)
+    # Tous les bookings de l'user où il a annulé sa participation
+    @bookings_cancelled = @user.bookings.joins(:event).where("events.active = ? AND bookings.cancelled = ?", true, true)
+    # Tous les bookings non archivés
+    @bookings_not_archived = @bookings_active_to_come
+    # Tous les bookings archivés
+    @bookings_archived = @bookings_active_past + @bookings_event_cancelled + @bookings_cancelled
+    raise
   end
 
   private
